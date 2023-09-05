5a07be3c87e8b897fc68280ef429ecd5df9eb349
roodhouse <rughjm@gmail.com>
2023-09-05 14:10:41 -0500

    homeScreen trending work

--- src/components/main/home/Trending.jsx
+++ src/components/main/home/Trending.jsx
@@ -1,4 +1,20 @@
 import React from 'react'
+import Data from '../../../data.json'
+
+// console.log(Data.map((show) => {
+//     console.log(show)
+// }))
+
+// const trendingNow = () => {
+//     Data.map((isAtrend) => {
+//         if ( isAtrend.isTrending ) {
+//             console.log(isAtrend.title)
+//             return (
+//                 isAtrend.title
+//             )
+//         }
+//     })
+// }
 
 function Trending() {
   return (
@@ -6,6 +22,24 @@ function Trending() {
         <div id="trendingContainer">
             <div id="trendingHeading" className='text-white text-xl leading-normal font-light tracking-[-0.312px]'>
                 <h2>Trending</h2>
+            </div>
+            <div id="trendingItemsContainer">
+                {
+                    Data.map((isAtrend) => {
+                        if ( isAtrend.isTrending ) {
+                            console.log(isAtrend.title)
+                            return (
+
+                                <div key={isAtrend.title} className='trendingContainer'>
+                                    <div className='trending' style={{backgroundImage: isAtrend.thumbnail.trending.small}}>
+                                        <p>{isAtrend.year}</p>
+                                    </div>
+                                </div>
+                                isAtrend.title
+                            )
+                        }
+                    })
+                }
             </div>
         </div>
     </>
