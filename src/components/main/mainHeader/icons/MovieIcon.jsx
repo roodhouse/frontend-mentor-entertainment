9ca72b089f86eb6c4522989845a4bdc7b2944155
roodhouse <rughjm@gmail.com>
2023-09-05 09:55:44 -0500

    movieIcon

--- src/components/main/mainHeader/icons/MovieIcon.jsx
+++ src/components/main/mainHeader/icons/MovieIcon.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import MoviePageIcon from '../../../../assets/icon-nav-movies.svg'
 
 function MovieIcon() {
   return (
-    <div>MovieIcon</div>
+    <>
+        <div id="movieIconContainer" className='w-4 h-4'>
+            <img src={MoviePageIcon} alt="Home Icon" />
+        </div>
+    </>
   )
 }
 
