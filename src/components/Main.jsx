992fb2315e721660d26fc1cf8093b2ba196049e2
roodhouse <rughjm@gmail.com>
2023-09-06 10:54:55 -0500

    refactoring of trending

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,24 +10,26 @@ function Main() {
             <div id="mainHeaderWrapper" className='xl:w-[96px] xl:h-[960px] xl:mr-9'>
                 <MainHeader />
             </div>
-            <div id="searchBarWrapper" className='xl:pt-8'>
-                <SearchBar />
+            <div id="searchAndContentWrapper">
+                <div id="searchBarWrapper" className='xl:pt-8'>
+                    <SearchBar />
+                </div>
+                <div id="homeWrapper">
+                    <Home />
+                </div>
+                {/* <div id="searchPageWrapper">
+                    <SearchPage />
+                </div>
+                <div id="moviesPageWrapper">
+                    <MoviesPage />
+                </div>
+                <div id="tvPageWrapper">
+                    <TvPage />
+                </div>
+                <div id="bookmarkedPageWrapper">
+                    <BookmarkedPage />
+                </div> */}
             </div>
-            <div id="homeWrapper">
-                <Home />
-            </div>
-            {/* <div id="searchPageWrapper">
-                <SearchPage />
-            </div>
-            <div id="moviesPageWrapper">
-                <MoviesPage />
-            </div>
-            <div id="tvPageWrapper">
-                <TvPage />
-            </div>
-            <div id="bookmarkedPageWrapper">
-                <BookmarkedPage />
-            </div> */}
 
         </div>
     </>
--- src/components/main/home/Trending.jsx
+++ src/components/main/home/Trending.jsx
@@ -12,87 +12,102 @@ function Trending() {
     const [ currentTransform, setCurrentTransform ] = useState(0)
 
     const handleSlide = (e) => {
+        // need to get container of all items...
+        const container = document.querySelector('.trendingItemsInnerContainer')
+        console.log(container)
         const containers = document.querySelectorAll('.trendingItemsInnerContainer')
         const clickedIndex = Array.from(containers).indexOf(e.currentTarget)
-        if ( window.innerWidth < 768 ) {
-            if (e.currentTarget.classList.contains('nextItem')) {
-                document.getElementById('trendingItemsContainer').style.transform = `translateX(${currentTransform  - 256}px)`;
-                setCurrentTransform(currentTransform - 256)
-                let prevFeature = containers[clickedIndex-1]
-                let newFeature = containers[clickedIndex]
-                let nextFeature = containers[clickedIndex+1]
-                
-                if ( containers[clickedIndex -2] ) {
-                    containers[clickedIndex -2].classList.remove('prevItem')
-                }
-                
-                
-                if ( nextFeature === undefined ) {
-                    nextFeature = containers[0]
-                    nextFeature.classList.add('nextItem')
-                    newFeature.classList.replace('nextItem', 'featureItem')
-                    prevFeature.classList.replace('featureItem', 'prevItem')
-    
-                    const clickListener = () => {
-                        nextFeature.classList.replace('nextItem', 'featureItem')
-                        containers[1].classList.add('nextItem')
-                        newFeature.classList.remove('featureItem')
-                        prevFeature.classList.remove('prevItem')
-                        document.getElementById('trendingItemsContainer').style.transform = `translateX(0)`;
-                        setCurrentTransform(0)
-    
-                        newFeature.removeEventListener('click', clickListener)
-                    }
-    
-                    newFeature.addEventListener('click', clickListener)
-                    
-                } else {
-                    prevFeature.classList.replace('featureItem', 'prevItem')
-                    newFeature.classList.replace('nextItem', 'featureItem')
-                    nextFeature.classList.add('nextItem')
-                }
-        }
-        } else {
-
-            if (e.currentTarget.classList.contains('nextItem')) {
-                document.getElementById('trendingItemsContainer').style.transform = `translateX(${currentTransform  - 510}px)`;
-                setCurrentTransform(currentTransform - 510)
-                let prevFeature = containers[clickedIndex-1]
-                let newFeature = containers[clickedIndex]
-                let nextFeature = containers[clickedIndex+1]
-                
-                if ( containers[clickedIndex -2] ) {
-                    containers[clickedIndex -2].classList.remove('prevItem')
-                }
-                
-                
-                if ( nextFeature === undefined ) {
-                    nextFeature = containers[0]
-                    nextFeature.classList.add('nextItem')
-                    newFeature.classList.replace('nextItem', 'featureItem')
-                    prevFeature.classList.replace('featureItem', 'prevItem')
-    
-                    const clickListener = () => {
-                        nextFeature.classList.replace('nextItem', 'featureItem')
-                        containers[1].classList.add('nextItem')
-                        newFeature.classList.remove('featureItem')
-                        prevFeature.classList.remove('prevItem')
-                        document.getElementById('trendingItemsContainer').style.transform = `translateX(0)`;
-                        setCurrentTransform(0)
-    
-                        newFeature.removeEventListener('click', clickListener)
-                    }
-    
-                    newFeature.addEventListener('click', clickListener)
-                    
-                } else {
-                    prevFeature.classList.replace('featureItem', 'prevItem')
-                    newFeature.classList.replace('nextItem', 'featureItem')
-                    nextFeature.classList.add('nextItem')
-                }
-        }
+        const containerArray = Array.from(containers)
+        console.log(containerArray)
+        const firstContainer = containerArray.shift()
+        console.log(containerArray)
+        containerArray.push(firstContainer)
+        console.log(containerArray)
+        container.innerHTML = ''
+        containers.forEach((container) => {
             
-        }
+        })
+
+
+        // if ( window.innerWidth < 768 ) {
+        //     if (e.currentTarget.classList.contains('nextItem')) {
+        //         document.getElementById('trendingItemsContainer').style.transform = `translateX(${currentTransform  - 256}px)`;
+        //         setCurrentTransform(currentTransform - 256)
+        //         let prevFeature = containers[clickedIndex-1]
+        //         let newFeature = containers[clickedIndex]
+        //         let nextFeature = containers[clickedIndex+1]
+                
+        //         if ( containers[clickedIndex -2] ) {
+        //             containers[clickedIndex -2].classList.remove('prevItem')
+        //         }
+                
+                
+        //         if ( nextFeature === undefined ) {
+        //             nextFeature = containers[0]
+        //             nextFeature.classList.add('nextItem')
+        //             newFeature.classList.replace('nextItem', 'featureItem')
+        //             prevFeature.classList.replace('featureItem', 'prevItem')
+    
+        //             const clickListener = () => {
+        //                 nextFeature.classList.replace('nextItem', 'featureItem')
+        //                 containers[1].classList.add('nextItem')
+        //                 newFeature.classList.remove('featureItem')
+        //                 prevFeature.classList.remove('prevItem')
+        //                 document.getElementById('trendingItemsContainer').style.transform = `translateX(0)`;
+        //                 setCurrentTransform(0)
+    
+        //                 newFeature.removeEventListener('click', clickListener)
+        //             }
+    
+        //             newFeature.addEventListener('click', clickListener)
+                    
+        //         } else {
+        //             prevFeature.classList.replace('featureItem', 'prevItem')
+        //             newFeature.classList.replace('nextItem', 'featureItem')
+        //             nextFeature.classList.add('nextItem')
+        //         }
+        // }
+        // } else {
+
+        //     if (e.currentTarget.classList.contains('nextItem')) {
+        //         document.getElementById('trendingItemsContainer').style.transform = `translateX(${currentTransform  - 510}px)`;
+        //         setCurrentTransform(currentTransform - 510)
+        //         let prevFeature = containers[clickedIndex-1]
+        //         let newFeature = containers[clickedIndex]
+        //         let nextFeature = containers[clickedIndex+1]
+                
+        //         if ( containers[clickedIndex -2] ) {
+        //             containers[clickedIndex -2].classList.remove('prevItem')
+        //         }
+                
+                
+        //         if ( nextFeature === undefined ) {
+        //             nextFeature = containers[0]
+        //             nextFeature.classList.add('nextItem')
+        //             newFeature.classList.replace('nextItem', 'featureItem')
+        //             prevFeature.classList.replace('featureItem', 'prevItem')
+    
+        //             const clickListener = () => {
+        //                 nextFeature.classList.replace('nextItem', 'featureItem')
+        //                 containers[1].classList.add('nextItem')
+        //                 newFeature.classList.remove('featureItem')
+        //                 prevFeature.classList.remove('prevItem')
+        //                 document.getElementById('trendingItemsContainer').style.transform = `translateX(0)`;
+        //                 setCurrentTransform(0)
+    
+        //                 newFeature.removeEventListener('click', clickListener)
+        //             }
+    
+        //             newFeature.addEventListener('click', clickListener)
+                    
+        //         } else {
+        //             prevFeature.classList.replace('featureItem', 'prevItem')
+        //             newFeature.classList.replace('nextItem', 'featureItem')
+        //             nextFeature.classList.add('nextItem')
+        //         }
+        // }
+            
+        // }
     }
 
     useEffect(() => {
